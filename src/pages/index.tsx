de5354c2e4e47349302d514ac32113ebc0aafbe5
yona3 <kou.y.0905@icloud.com>
2021-09-22 00:30:20 +0900

    🔍 Add Head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 
 import { Layout } from "../components/Layout";
 
 const Index: NextPage = () => {
   return (
     <Layout>
+      <Head>
+        <title>Home</title>
+        <meta
+          name="description"
+          content="This is my Next.js + TypeScript + Tailwind CSS starter template."
+        />
+      </Head>
+
       <div className="text-center">
         <p className="mt-12 text-xl text-blue-500">
           Next.js + TypeScript + Tailwind CSS{" "}
