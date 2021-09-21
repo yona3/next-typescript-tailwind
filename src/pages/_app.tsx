7794d5c62e52156bcd97d0e8dba43e3cf3ea398c
yona3 <kou.y.0905@icloud.com>
2021-09-22 00:22:43 +0900

    ⬆️ Upgrade dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "tailwindcss/tailwind.css";
 
-import type { AppProps } from "next/dist/next-server/lib/router/router";
+import type { AppProps } from "next/dist/shared/lib/router/router";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
