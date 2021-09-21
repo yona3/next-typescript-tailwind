87140dc355b27a4ef0d32463658c6a6c353691fd
yona3 <kou.y.0905@icloud.com>
2021-09-22 01:01:12 +0900

    ♿️ Update html lang

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,9 @@
-import type { DocumentContext } from "next/document";
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return initialProps;
-  }
-
   render(): JSX.Element {
-    const { locale } = this.props.__NEXT_DATA__;
-
     return (
-      <Html lang={locale}>
+      <Html lang="en">
         <Head />
         <body>
           <Main />
